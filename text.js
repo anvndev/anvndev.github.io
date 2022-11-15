847ce069bac450ce7e58ddc19fb2fe7572f75a6c
Anvndev <83855206+anvndev@users.noreply.github.com>
2022-11-16 02:41:48 +0700

    up

---
+++ text.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#elem', {
+    // Waits 1000ms after typing "First"
+    strings: ['Anvndev','Software Engineer','Designer','Web Developer'],
+    typeSpeed: 50,
+    backSpeed: 30,
+    loop: true,
+});  
+    // Run Andev
