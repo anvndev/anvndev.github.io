f8c76271f33879936421ee5669d773412969e5b5
An Van Nguyen <anvndev@gmail.com>
2024-03-03 16:41:26 +0700

    update fx -v1

--- text.js
+++ text.js
@@ -1,6 +1,6 @@
 var typed = new Typed('#elem', {
     // Waits 1000ms after typing "First"
-    strings: ['Anvndev','Software Engineer','Designer', 'M-A Developer'],
+    strings: ['Anvndev','Software Engineer','Designer'],
     typeSpeed: 50,
     backSpeed: 30,
     loop: true,
